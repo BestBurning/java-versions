c5a1f6a18579f7a254fd545babd2a0e5239f4d17
shea <zhushuai026@gmail.com>
2022-11-25 11:25:05 +0800

    feat: 13 文本块加入变量标识

--- java-13/src/main/java/com/di1shuai/java13/string/StringBlockDemo.java
+++ java-13/src/main/java/com/di1shuai/java13/string/StringBlockDemo.java
@@ -3,10 +3,8 @@ package com.di1shuai.java13.string;
 /**
  * @author Shea
  * @date 2021-01-22
- * @description
- *
- * """
- *
+ * @description """
+ * <p>
  * """
  * 来书写多行文本
  */
@@ -14,26 +12,32 @@ public class StringBlockDemo {
 
     public static void main(String[] args) {
         // < 13
-        String htmlBefore13 =   "<html>\n" +
-                                "    <body>\n" +
-                                "        <p>Hello, Java 13</p>\n" +
-                                "    </body>\n" +
-                                "</html>\n";
+        String htmlBefore13 = "<html>\n" +
+                "    <body>\n" +
+                "        <p>Hello, Java 13</p>\n" +
+                "    </body>\n" +
+                "</html>\n";
         System.out.println(htmlBefore13);
 
         //13
         String html13 = """
-                        <html>
-                            <body>
-                                <p>Hello,  Java 13</p>
-                            </body>
-                        </html>
-                        """;
-
+                <html>
+                    <body>
+                        <p>Hello,  Java 13</p>
+                    </body>
+                </html>
+                """;
         System.out.println(html13);
+        String html13Var = """
+                 <html>
+                    <body>
+                        <p>Hello,  %s %d</p>
+                    </body>
+                </html>
+                                
+                """.formatted("Java", 13);
 
-
-
+        System.out.println(html13Var);
 
     }
 
