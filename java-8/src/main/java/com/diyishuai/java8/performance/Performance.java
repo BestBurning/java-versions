d87287ab10bd33289f942d77d8f8a8e415e39454
shea <zhushuai026@gmail.com>
2022-11-25 11:21:28 +0800

    feat: 性能jmh测试

--- java-8/src/main/java/com/diyishuai/java8/performance/Performance.java
+++ java-8/src/main/java/com/diyishuai/java8/performance/Performance.java
@@ -1,11 +1,14 @@
 package com.diyishuai.java8.performance;
 
 import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.results.format.ResultFormatType;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,9 +24,16 @@ import java.util.concurrent.TimeUnit;
 @Threads(2)
 public class Performance {
 
+    @State(Scope.Thread)
+    public static class MyState {
+        public long a = 455565655225562L;
+
+        public String url = "http://localhost:8888/";
+    }
+
     @Benchmark
-    public long div() {
-        long t = 455565655225562L;
+    public long cpu(MyState state) {
+        long t = state.a;
         long a = 0;
         for (int i = 0; i < 1000; i++) {
             a = t / 1024 / 1024 / 1024;
@@ -31,4 +41,75 @@ public class Performance {
         return a;
     }
 
+
+
+
+
+
+
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+    @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+    @Benchmark
+    public String io(MyState state) {
+        return doGet(state.url);
+    }
+
+
+    public static String doGet(String httpurl) {
+        HttpURLConnection connection = null;
+        InputStream is = null;
+        BufferedReader br = null;
+        String result = null;// 返回结果字符串
+        try {
+            // 创建远程url连接对象
+            URL url = new URL(httpurl);
+            // 通过远程url连接对象打开一个连接，强转成httpURLConnection类
+            connection = (HttpURLConnection) url.openConnection();
+            // 设置连接方式：get
+            connection.setRequestMethod("GET");
+            // 设置连接主机服务器的超时时间：15000毫秒
+            connection.setConnectTimeout(15000);
+            // 设置读取远程返回的数据时间：60000毫秒
+            connection.setReadTimeout(60000);
+            // 发送请求
+            connection.connect();
+            // 通过connection连接，获取输入流
+            if (connection.getResponseCode() == 200) {
+                is = connection.getInputStream();
+                // 封装输入流is，并指定字符集
+                br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                // 存放数据
+                StringBuffer sbf = new StringBuffer();
+                String temp = null;
+                while ((temp = br.readLine()) != null) {
+                    sbf.append(temp);
+                    sbf.append("\r\n");
+                }
+                result = sbf.toString();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            if (null != br) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            connection.disconnect();// 关闭远程连接
+        }
+        return result;
+    }
 }
